5690408488ca3f45806478ec2fdb5e682097c4e1
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-08-21 21:22:18 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,24 +1,12 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoundexTestSuite, GeneratesCorrectCodes) {
+TEST(SoundexTestSuite, GeneratesCorrectSoundexCodes) {
     char soundex[5];
-
-    generateSoundex("AX", soundex);
-    ASSERT_STREQ(soundex, "A200");
-
-    generateSoundex("HERMAN", soundex);
-    ASSERT_STREQ(soundex, "H655");
-
-    generateSoundex("MAHUL", soundex);
-    ASSERT_STREQ(soundex, "M040");
-
-    generateSoundex("Ashcraft", soundex);
-    ASSERT_STREQ(soundex, "A226");
-
-    generateSoundex("Robert", soundex);
-    ASSERT_STREQ(soundex, "R163");
-
-    generateSoundex("Bbbb", soundex);
-    ASSERT_STREQ(soundex, "B100");
+    
+    generateSoundex("AX", soundex);       ASSERT_STREQ(soundex, "A200");
+    generateSoundex("HERMAN", soundex);   ASSERT_STREQ(soundex, "H655");
+    generateSoundex("MAHUL", soundex);    ASSERT_STREQ(soundex, "M040");
+    generateSoundex("Ashcraft", soundex); ASSERT_STREQ(soundex, "A226");
+    generateSoundex("Robert", soundex);   ASSERT_STREQ(soundex, "R163");
 }
